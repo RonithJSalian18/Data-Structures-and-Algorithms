555959b367d1ab5ddb818415a899670f2462e1e5
Ronith J Salian <ronithjsalian01@gmail.com>
2026-07-04 09:10:36 +0530

    Time: 0 ms (100%), Space: 19.5 MB (53.97%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,3 +1,25 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+
+        # Fast exponentiation using recursion
+        def power(x, n):
+            # Base case
+            if n == 0:
+                return 1
+
+            # Compute x^(n//2)
+            half = power(x, n // 2)
+
+            # If n is even: x^n = (x^(n/2))²
+            if n % 2 == 0:
+                return half * half
+            # If n is odd: x^n = (x^(n//2))² * x
+            else:
+                return half * half * x
+
+        # Handle negative exponent
+        if n < 0:
+            return 1 / power(x, -n)
+
+        # Compute positive exponent
+        return power(x, n)
