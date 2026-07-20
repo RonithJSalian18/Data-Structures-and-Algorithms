c791f34576606c272c03915106158acedb4c80e2
Ronith J Salian <ronithjsalian01@gmail.com>
2026-07-20 16:37:24 +0530

    Time: 136 ms (99.18%), Space: 20.6 MB (41.76%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,19 +1,19 @@
-from math import ceil
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def works(k):                              # Check if speed k is sufficient
+        l, r = 1, max(piles)              # Search range of possible eating speeds
+        res = r                           # Store the minimum valid speed found
+
+        while l <= r:
+            k = (l + r) // 2              # Try the middle eating speed
             hours = 0
+
             for p in piles:
-                hours += ceil(p / k)               # Hours needed for this pile
-            return hours <= h                      # Can finish within h hours?
+                hours += ceil(p / k)      # Hours needed to finish this pile
 
-        l, r = 1, max(piles)                       # Search range of eating speeds
-        while l < r:
-            k = (l + r) // 2                       # Middle eating speed
-            if works(k):
-                r = k                              # Try a smaller valid speed
+            if hours <= h:                # Can finish within h hours
+                r = k - 1                 # Search for a smaller valid speed
+                res = min(res, k)         # Update the answer
             else:
-                l = k + 1                          # Increase the speed
+                l = k + 1                 # Speed is too slow, increase it
 
-        return l                                   # Minimum valid eating speed
+        return res                        # Minimum eating speed
