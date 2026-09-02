d8766b57b7c2f2e734151727ccb5aa3f2d445abe
Ronith J Salian <ronithjsalian01@gmail.com>
2026-09-02 17:47:52 +0530

    Time: 499 ms (65.07%), Space: 19.6 MB (78.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,20 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort()                          # Sort coins for early stopping
-        dp = [0] * (amount + 1)               # dp[i] = min coins needed for amount i
+        coins.sort()
+        dp = [0] * (amount + 1)
 
         for i in range(1, amount + 1):
-            minn = float('inf')               # Initialize minimum coins
+            mini = float('inf')
 
             for coin in coins:
                 diff = i - coin
-                if diff < 0:                  # No need to check larger coins
+                if diff < 0:
                     break
-                minn = min(minn, 1 + dp[diff])  # Choose the best option
-
-            dp[i] = minn                      # Store minimum coins for amount i
+                mini = min(mini, dp[diff] + 1)
+            dp[i] = mini
 
         if dp[amount] < float('inf'):
-            return dp[amount]                 # Solution exists
+            return dp[amount]
         else:
-            return -1                         # Impossible to make the amount
+            return -1 
