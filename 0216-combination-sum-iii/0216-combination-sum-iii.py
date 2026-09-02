a283c5636bd6cfe8d8f7c1584d1c7bd0364b42f5
Ronith J Salian <ronithjsalian01@gmail.com>
2026-09-02 19:45:39 +0530

    Time: 4 ms (8.07%), Space: 19.3 MB (34.36%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(i, cur, score):
+            if score == n and cur[:] not in res and len(cur) == k:
+                res.append(cur[:])
+                return
+
+            if i > 9 or score > n:
+                return
+
+            cur.append(i)
+            backtrack(i+1, cur, score + i)
+            cur.pop()
+
+            backtrack(i+1, cur, score)
+
+        backtrack(1, [], 0)
+        return res
