0a4154c8e0e9ae053384969e7ed673b3340f3e21
Ronith J Salian <ronithjsalian01@gmail.com>
2026-09-10 16:07:41 +0530

    Time: 0 ms (100%), Space: 43.2 MB (51.16%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,14 +1,6 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        hashMap = {}
-        n = len(edges)
+        a, b = edges[0]
+        c, d = edges[1]
 
-        for i in range(len(edges)):
-            for j in range(len(edges[i])):
-                v = edges[i][j]
-                hashMap[v] = 1 + hashMap.get(v, 0)
-
-        for val, freq in hashMap.items():
-            if freq == n:
-                return val
-        
+        return a if a == c or a == d else b
